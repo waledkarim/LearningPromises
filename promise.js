00ddfc233db2074405704481be620cd41877d515
Waled Karim <waledkarim445@gmail.com>
2024-10-10 21:08:46 +0600

    Another promise constructor

--- promise.js
+++ promise.js
@@ -17,3 +17,10 @@ new Promise((resolve) => {
     setTimeout(() => resolve("Done!"), 1000);
 })
 .then(result => console.log(result))
+
+new Promise((resolve) => {
+    console.log(1);
+    resolve(2);
+}).then(result => console.log(result))
+
+console.log(3);
